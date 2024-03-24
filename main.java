e1a6dc4ad78f9a7cf0b487bad04ad1e8db8b5159
sohnjiye <81518256+sohnjiye@users.noreply.github.com>
2024-03-24 15:25:30 +0900

    Add files via upload

---
+++ main.java
@@ -0,0 +1,11 @@
+package reservation;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		Reserve PRS = new Reserve(); // Reserve 클래스의 객체 생성 
+		PRS.run(); // PRS의 run() 메소드를 실행
+	}
+}
+
